edcb285bb1471daf950f5fa5352f929a17d39561
Alfian57 <alfiangadingsaputra@gmail.com>
2024-12-30 23:34:00 +0700

    ci: add ci pipeline

--- config.php
+++ config.php
@@ -1,15 +1,22 @@
 <?php
-$host = 'localhost'; // Host database
-$db   = 'db_bookcourt'; // Nama database
-$user = 'root'; // Username database
-$pass = ''; // Password database
+if (file_exists('.env')) {
+    $env = parse_ini_file('.env');
+    $dbHost = $env["DB_HOST"];
+    $dbUsername = $env["DB_USERNAME"];
+    $dbPassword = $env["DB_PASSWORD"];
+    $dbName = $env["DB_NAME"];
+} else {
+    $dbHost = getenv("DB_HOST");
+    $dbUsername = getenv("DB_USERNAME");
+    $dbPassword = getenv("DB_PASSWORD");
+    $dbName = getenv("DB_NAME");
+}
 
 try {
     // Membuat koneksi ke database
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName;charset=utf8", $dbUsername, $dbPassword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Mengatur mode error untuk PDO
 } catch (PDOException $e) {
     // Menangkap exception jika koneksi gagal
     echo "Koneksi gagal: " . $e->getMessage();
 }
-?>
--- config2.php
+++ config2.php
@@ -1,12 +1,19 @@
 <?php
-// Database configuration
-define('DB_HOST', 'localhost');
-define('DB_USER', 'your_username'); // Replace with your database username
-define('DB_PASS', 'your_password'); // Replace with your database password
-define('DB_NAME', 'db_bookcourt'); // Replace with your database name
+if (file_exists('.env')) {
+    $env = parse_ini_file('.env');
+    $dbHost = $env["DB_HOST"];
+    $dbUsername = $env["DB_USERNAME"];
+    $dbPassword = $env["DB_PASSWORD"];
+    $dbName = $env["DB_NAME"];
+} else {
+    $dbHost = getenv("DB_HOST");
+    $dbUsername = getenv("DB_USERNAME");
+    $dbPassword = getenv("DB_PASSWORD");
+    $dbName = getenv("DB_NAME");
+}
 
 // MySQLi connection for procedural style
-$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$mysqli = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
 // Check MySQLi connection
 if ($mysqli->connect_error) {
@@ -15,10 +22,9 @@ if ($mysqli->connect_error) {
 
 // PDO connection for PDO style
 try {
-    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8", DB_USER, DB_PASS);
+    $pdo = new PDO("mysql:host=" . $dbHost . ";dbname=" . $dbName . ";charset=utf8", $dbUsername, $dbPassword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Set error mode to exception
 } catch (PDOException $e) {
     // Catch exception if connection fails
     die("Koneksi PDO gagal: " . $e->getMessage());
 }
-?>
